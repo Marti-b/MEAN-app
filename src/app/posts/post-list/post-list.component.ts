d0dc350db8bdd06d642514ef63439b6a1f57abc1
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-11-12 10:47:33 +0100

    Using Structural Directives (ngIf, ngFor)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,11 +8,12 @@ import { title } from 'process';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  post = [
+ /* posts = [
     { title: 'First Post', content: 'This is the first post\'s content ' },
-    { title: 'Second Post', content: 'This is the first post\'s content ' },
-    { title: 'Third Post', content: 'This is the first post\'s content ' }
-  ];
+    { title: 'Second Post', content: 'This is the second post\'s content ' },
+    { title: 'Third Post', content: 'This is the third post\'s content ' }
+  ]; */
+    posts =[];
 
   constructor() { }
 
