d2e0c60803167fb524fd98133da4857575dfda36
Marti-b <46929576+Marti-b@users.noreply.github.com>
2020-12-09 22:05:57 +0100

    Connecting Express to MongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const Post = require('./models/post');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://Bali:tCkMLYgPeSeW9QbV@cluster0.3bkjr.mongodb.net/<dbname>?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to database');
+})
+.catch (() => {
+  console.log('Connection failed')
+});
+
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
-})
+});
 
 app.post('/api/posts', (req, res, next) => {
   const post = new Post({
